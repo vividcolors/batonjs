94788b5f329ec6763d55845cf50792900072863b
vc-tyam <tyam@vividcolors.co.jp>
2023-03-30 20:40:17 +0900

    add doc generator

---
+++ doc/script/convert.js
@@ -0,0 +1,79 @@
+import {baton} from '../../src/baton.js'
+import { readFile, writeFile, opendir, copyFile, stat, mkdir } from 'node:fs/promises'
+import { JSDOM } from 'jsdom'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const show = (config) => {
+  return {
+    title: (el) => ({innerHTML: el.innerHTML + ' | ' + config.siteTitle}), 
+    head: (el) => ({innerHTML: el.innerHTML + config.head}), 
+    "#header": {innerHTML: config.header}, 
+    "#sidebar": {innerHTML: config.sidebar}, 
+    "#footer": {innerHTML: config.footer}, 
+    "#toc": {innerHTML: config.toc}
+  }
+}
+
+const buildToc = (lang, dom, config) => {
+  const ul = config.window.document.documentElement.querySelector(`.toc.${lang}`).content.firstElementChild.cloneNode(true)
+  const hds = dom.window.document.querySelectorAll('h2[id]')
+  const document = dom.window.document
+  for (let hd of hds) {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    a.href = "#" + hd.id
+    a.textContent = hd.textContent
+    li.appendChild(a)
+    ul.appendChild(li)
+  }
+  return ul.outerHTML
+}
+
+const run1 = async (lang, config, inPath, outPath) => {
+  const dom = await readFile(inPath).then(str => (new JSDOM(str)))
+
+  const state =  {
+    siteTitle: config.window.document.documentElement.querySelector(`.site-title.${lang}`).innerHTML, 
+    head: config.window.document.documentElement.querySelector(`.head.${lang}`).innerHTML, 
+    header: config.window.document.documentElement.querySelector(`.header.${lang}`).innerHTML, 
+    sidebar: config.window.document.documentElement.querySelector(`.sidebar.${lang}`).innerHTML, 
+    footer: config.window.document.documentElement.querySelector(`.footer.${lang}`).innerHTML, 
+    toc: buildToc(lang, dom, config)
+  }
+  baton(state, show, dom.window.document)
+
+  const output = dom.serialize()
+  console.log('out: ', outPath)
+  await writeFile(outPath, output)
+}
+
+const runDir = async (lang, config, inDirPath, outDirPath) => {
+  for await (let e of await opendir(inDirPath)) {
+    const inPath = path.join(inDirPath, e.name)
+    const outPath = path.join(outDirPath, e.name)
+    if (e.isDirectory()) {
+      await stat(outPath).catch(() => {
+        mkdir(outPath)
+      })
+      await runDir(lang, config, inPath, outPath)
+    } else if (inPath.endsWith('.html')) {
+      await run1(lang, config, inPath, outPath)
+    } else {
+      await copyFile(inPath, outPath)
+    }
+  }
+}
+
+const run = async () => {
+  const docPath = path.join(path.dirname(fileURLToPath(import.meta.url)), "..")
+  const configPath = path.join(docPath, 'config.html')
+  const config = await readFile(configPath).then(str => (new JSDOM(str)))
+
+  const inDirPath = path.join(docPath, "src")
+  const outDirPath = path.join(docPath, "dist")
+  await runDir("ja", config, inDirPath, outDirPath)
+}
+
+
+await run()
